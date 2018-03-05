56663395f623a588fb3a5d6b7d557ec280de3a88
vyadav06 <vyadav@stevens.edu>
2018-03-05 01:03:11 -0500

    roll dice functionality and set current values for both player to zero

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
@@ -6,9 +6,32 @@ activePlayer = 1;
 
 //dice
 
-dice = Math.floor(Math.random() * 6) + 1;
-//document.querySelector('#current-' + activePlayer).textContent= dice;
-document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+document.querySelector('.dice').style.display = 'none';
 
-var x = document.querySelector('#current-0').textContent;
-console.log(x);
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function (){
+    // random number
+   var dice = Math.floor(Math.random() * 6) + 1;
+    //display the result using variable
+    //make the dice visible
+   
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    
+    // change the src of image
+    
+    diceDOM.src = 'dice-' + dice + '.png';
+    
+});
+
+
+//document.querySelector('#current-' + activePlayer).textContent= dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+//var x = document.querySelector('#current-0').textContent;
+//console.log(x);
